e3d3401c56bc430079fbbf24115a64f9cb4bf067
devisbrian <otienonduko@gmail.com>
2024-04-30 16:47:57 +0300

    few adds

--- src/main/java/com/example/gsconsumingrest/GsConsumingRestApplication.java
+++ src/main/java/com/example/gsconsumingrest/GsConsumingRestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GsConsumingRestApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GsConsumingRestApplication.class, args);
 	}
 
